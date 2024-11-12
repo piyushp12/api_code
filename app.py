94343b1db11f52f0d74b3014975569560c187bf3
Piyush <96821378+piyushp12@users.noreply.github.com>
2024-11-12 15:07:23 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -64,9 +64,6 @@ def generate_doc_route():
     
     return generate_document(template_file, json_data)
 
-if __name__ == '__main__':
-    app.run(debug=True)
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
