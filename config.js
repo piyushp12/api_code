d17db8f773e60f144476b0daa29b389fb874e725
Piyush <96821378+piyushp12@users.noreply.github.com>
2024-11-12 11:22:12 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: 'doc-app',
+    script: 'app.py',
+    interpreter: 'python3',
+    exec_mode: 'fork',
+    instances: 1,
+    watch: true,
+    max_memory_restart: "1G",
+  };   
+                             
+    
